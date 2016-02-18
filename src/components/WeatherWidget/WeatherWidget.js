e96be11562cbd88a83a3df376ad19f824bea48e5
insaineyesay <insaineyesay@gmail.com>
2016-02-17 23:14:49 -0500

    url call working. need to get it out of function scope

--- src/components/WeatherWidget/WeatherWidget.js
+++ src/components/WeatherWidget/WeatherWidget.js
@@ -1,21 +1,43 @@
 import React, {Component} from 'react';
+// const httpRequest = new XMLHttpRequest();
+
 
 export default class WeatherWidget extends Component {
 
   componentDidMount() {
+    const defineResponse = {};
     this.pullData();
   }
 
-  pullData() {
+
+  pullData = function pullData() {
     const httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+      if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+          console.log(JSON.parse(httpRequest.responseText));
+          const thing = JSON.parse(httpRequest.responseText);
+          console.log(thing);
+          defineResponse(thing);
+          console.log('wtf');
+        } else {
+          console.error(httpRequest.responseText);
+        }
+      }
+    };
     httpRequest.open(
       'GET',
       'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22virginia%20beach%2C%20va%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys',
       true
     );
-    if (httpRequest.status === 200) {
-      console.log('yay');
-    }
+
+    httpRequest.send();
+    console.log(httpRequest.status);
+  }
+
+
+  alertContents = function alertContents() {
+
   }
 
   render() {
@@ -25,4 +47,9 @@ export default class WeatherWidget extends Component {
               </div>
           );
   }
+
+
 }
+WeatherWidget.propTypes = {
+  url: React.PropTypes.string.isRequired
+};
